0345ed4818f9efe88186cea1a89409ebadae7365
Ben Tideswell <ben@fishpig.co.uk>
2022-12-15 12:12:43 +0000

    1.0.1

--- Mock/Magento/AdminNotification/Model/Inbox.php
+++ Mock/Magento/AdminNotification/Model/Inbox.php
@@ -7,4 +7,75 @@ namespace Magento\AdminNotification\Model;
 
 class Inbox
 {
+    /**
+     *
+     */
+    public function getSeverities($severity = null)
+    {
+        return [];
+    }
+
+    /**
+     *
+     */
+    public function loadLatestNotice()
+    {
+        return $this;
+    }
+
+    /**
+     *
+     */
+    public function getNoticeStatus()
+    {
+        return '';
+    }
+
+    /**
+     *
+     */
+    public function parse(array $data)
+    {
+        return $this;
+    }
+
+    /**
+     *
+     */
+    public function add($severity, $title, $description, $url = '', $isInternal = true)
+    {
+        return $this;
+    }
+
+    /**
+     *
+     */
+    public function addCritical($title, $description, $url = '', $isInternal = true)
+    {
+        return $this;
+    }
+
+    /**
+     *
+     */
+    public function addMajor($title, $description, $url = '', $isInternal = true)
+    {
+        return $this;
+    }
+
+    /**
+     *
+     */
+    public function addMinor($title, $description, $url = '', $isInternal = true)
+    {
+        return $this;
+    }
+
+    /**
+     *
+     */
+    public function addNotice($title, $description, $url = '', $isInternal = true)
+    {
+        return $this;
+    }
 }
